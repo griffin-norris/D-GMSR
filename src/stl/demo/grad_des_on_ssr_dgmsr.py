6894fb5fd1ccdd0b58c78834921584004deb50eb
Griffin Norris <norrisg@student.ethz.ch>
2024-06-10 13:43:49 -0700

    move 2nd version of `grad_des_on_ssr_dgmsr` to own file

--- src/stl/demo/grad_des_on_ssr_dgmsr.py
+++ src/stl/demo/grad_des_on_ssr_dgmsr.py
@@ -47,7 +47,9 @@ def grad_des_on_ssr_dgmsr():
         f_list_and_1.append(h_and)
         x_list_and_1.append(xk_and_1)
         
+        # h_and, c_i_w_i = gmsr_and(eps, 32, weights, xk_and_2)
         h_and, c_i_w_i = f_func_smth(xk_and_2)[0,0], A_func_smth(xk_and_2)[0,:]
+
         xk_and_2 = xk_and_2 + alpha_and * c_i_w_i
         f_list_and_2.append(h_and)
         x_list_and_2.append(xk_and_2)
@@ -75,75 +77,75 @@ def grad_des_on_ssr_dgmsr():
     f_list_or_2 = np.vstack(f_list_or_2)
 
     # ---------------------------------------------------------------
-    # Results - And
+    # Results - D-SSR
 
     fig, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 2]}, figsize=(8, 6))
-
-    color_1 = iter(cm.rainbow(np.linspace(0, 1, N)))
-    plt.figure()
-    for i in range(K):
-        c = next(color_1)
-        axs[0,0].plot(x_list_and_2[:, N-1-i], c=c)
-    axs[0,0].axhline(0, c='black', linestyle='dashed')
-    axs[0,0].set_xlabel(r'iterations', fontsize=16)
-    axs[0,0].set_ylabel('$f_i(x)$', fontsize=16)
-    axs[0,0].set_title('D-SSR', fontsize=16)
 
     color_1 = iter(cm.rainbow(np.linspace(0, 1, K)))
     plt.figure()
     for i in range(K):
         c = next(color_1)
-        axs[0,1].plot(x_list_and_1[:, K-1-i], c=c)
-    axs[0,1].axhline(0, c='black', linestyle='dashed')
-    axs[0,1].set_xlabel(r'iterations', fontsize=16)
-    axs[0,1].set_ylabel('$f_i(x)$', fontsize=16)
-    axs[0,1].set_title('D-GMSR', fontsize=16)
+        axs[0,0].plot(x_list_and_2[:, K-1-i], c=c)
+    axs[0,0].axhline(0, c='black', linestyle='dashed')
+    axs[0,0].set_xlabel(r'iterations', fontsize=16)
+    axs[0,0].set_ylabel('$f_i(x_k)$', fontsize=16)
+    axs[0,0].set_title('Conjuction - Always', fontsize=16)
 
     axs[1,0].plot(f_list_and_2, c='blue')
     axs[1,0].axhline(0, c='black', linestyle='dashed')
     axs[1,0].set_xlabel(r'iterations', fontsize=16)
-    axs[1,0].set_ylabel('$\\widetilde{\\min} ( y )$', fontsize=16)
-
-    axs[1,1].plot(f_list_and_1, c='blue')
-    axs[1,1].axhline(0, c='black', linestyle='dashed')
-    axs[1,1].set_xlabel(r'iterations', fontsize=16)
-    axs[1,1].set_ylabel('$^{\\wedge} h_{p, w}^{\\epsilon}( y )$', fontsize=16)
-
-    if save_fig: fig.savefig('figs/and_ssr_dgmsr.' + fig_format, bbox_inches='tight', dpi=fig_png_dpi)  
-
-    # ---------------------------------------------------------------
-    # Results - Or
-
-    fig, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 2]}, figsize=(8, 6))
- 
-    color_1 = iter(cm.rainbow(np.linspace(0, 1, K)))
-    plt.figure()
-    for i in range(K):
-        c = next(color_1)
-        axs[0,0].plot(x_list_or_2[:, K-1-i], c=c)
-    axs[0,0].axhline(0, c='black', linestyle='dashed')
-    axs[0,0].set_xlabel(r'iterations', fontsize=16)
-    axs[0,0].set_ylabel('$f_i(x)$', fontsize=16)
-    axs[0,0].set_title('D-SSR', fontsize=16)
+    axs[1,0].set_ylabel('$\\tilde{\\rho}^{\\varphi_1 \\wedge \\varphi_2 \\wedge \\dots \\wedge \\varphi_5} (x,k)$', fontsize=16)
 
     color_1 = iter(cm.rainbow(np.linspace(0, 1, K)))
     plt.figure()
     for i in range(K):
         c = next(color_1)
-        axs[0,1].plot(x_list_or_1[:, N-1-i], c=c)
+        axs[0,1].plot(x_list_or_2[:, K-1-i], c=c)
     axs[0,1].axhline(0, c='black', linestyle='dashed')
     axs[0,1].set_xlabel(r'iterations', fontsize=16)
-    axs[0,1].set_ylabel('$f_i(x)$', fontsize=16)
-    axs[0,1].set_title('D-GMSR', fontsize=16)
+    axs[0,1].set_ylabel('$f_i(x_k)$', fontsize=16)
+    axs[0,1].set_title('Disjunction - Eventually', fontsize=16)
 
-    axs[1,0].plot(f_list_or_2, c='blue')
+    axs[1,1].plot(f_list_or_2, c='blue')
+    axs[1,1].axhline(0, c='black', linestyle='dashed')
+    axs[1,1].set_xlabel(r'iterations', fontsize=16)
+    axs[1,1].set_ylabel('$\\tilde{\\rho}^{\\varphi_1 \\vee \\varphi_2 \\vee \\dots \\vee \\varphi_5} (x,k)$', fontsize=16)
+
+    if save_fig: fig.savefig('figs/dssr.' + fig_format, bbox_inches='tight', dpi=fig_png_dpi)
+
+    # ---------------------------------------------------------------
+    # Results - D-GMSR
+
+    fig, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 2]}, figsize=(8, 6))
+
+    color_1 = iter(cm.rainbow(np.linspace(0, 1, K)))
+    plt.figure()
+    for i in range(K):
+        c = next(color_1)
+        axs[0,0].plot(x_list_and_1[:, K-1-i], c=c)
+    axs[0,0].axhline(0, c='black', linestyle='dashed')
+    axs[0,0].set_xlabel(r'iterations', fontsize=16)
+    axs[0,0].set_ylabel('$f_i(x_k)$', fontsize=16)
+    axs[0,0].set_title('Conjuction - Always', fontsize=16)
+
+    axs[1,0].plot(f_list_and_1, c='blue')
     axs[1,0].axhline(0, c='black', linestyle='dashed')
     axs[1,0].set_xlabel(r'iterations', fontsize=16)
-    axs[1,0].set_ylabel('$$\\widetilde{\\max} ( y )$$', fontsize=14)
+    axs[1,0].set_ylabel('$\\Gamma^{\\varphi_1 \\wedge \\varphi_2 \\wedge \\dots \\wedge \\varphi_5}_{\\bm{\\epsilon}, \\bm{p}, \\bm{w}} (x,k)$', fontsize=16)
 
+    color_1 = iter(cm.rainbow(np.linspace(0, 1, K)))
+    plt.figure()
+    for i in range(K):
+        c = next(color_1)
+        axs[0,1].plot(x_list_or_1[:, K-1-i], c=c)
+    axs[0,1].axhline(0, c='black', linestyle='dashed')
+    axs[0,1].set_xlabel(r'iterations', fontsize=16)
+    axs[0,1].set_ylabel('$f_i(x_k)$', fontsize=16)
+    axs[0,1].set_title('Disjunction - Eventually', fontsize=16)
+ 
     axs[1,1].plot(f_list_or_1, c='blue')
     axs[1,1].axhline(0, c='black', linestyle='dashed')
     axs[1,1].set_xlabel(r'iterations', fontsize=16)
-    axs[1,1].set_ylabel('$^{\\vee} h_{p, w}^{\\epsilon}(y)$', fontsize=16)
+    axs[1,1].set_ylabel('$\\Gamma^{\\varphi_1 \\vee \\varphi_2 \\vee \\dots \\vee \\varphi_5}_{\\bm{\\epsilon}, \\bm{p}, \\bm{w}} (x,k)$', fontsize=16)
 
-    if save_fig: fig.savefig('figs/or_ssr_dgmsr.' + fig_format, bbox_inches='tight', dpi=fig_png_dpi)  
+    if save_fig: fig.savefig('figs/dgmsr.' + fig_format, bbox_inches='tight', dpi=fig_png_dpi)
